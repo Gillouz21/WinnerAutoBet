4278c5c3fe7c0b22531ec2690c09e56dead5af9c
Gillouz21 <gil210904@gmail.com>
2023-11-27 22:28:37 +0200

    fixed simulation output

--- simulation.py
+++ simulation.py
@@ -24,25 +24,26 @@ def game_series_generator(pool_of_money, multiplier, earnings_expected):
 def main():
     load_dotenv()
     df = pd.read_csv(os.getenv('DATA_PATH'))
-    debt = 0        #losses
+    debt = 10        #losses
     max_debt = 0    #max loss
     round = 0
     wins = 0
     for row in df.itertuples():
         mult = row.Multiplier
         is_win = row.isWin
-        debt += calculate_stake_size(debt, mult, 21)
+        debt += calculate_stake_size(debt, mult, 20)
 
         round += 1
         if debt > max_debt:
             max_debt = debt
 
+        print("round {} - current debt: {}, max debt: {} ".format(round, debt, max_debt))
+        
         if is_win:
             debt = 0
             round = 0
             wins += 1
 
-        print("round {} - current debt: {}, max debt: {} ".format(round, debt, max_debt))
 
     print(wins)
 if __name__ == "__main__":
